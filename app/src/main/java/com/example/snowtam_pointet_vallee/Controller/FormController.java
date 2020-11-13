0f8a9d1d96ba6f64433c5a7d90ba3a799cf6bd71
Lilian.V <lilian.vallee1108@gmail.com>
2020-11-13 15:36:12 +0100

    FormController Creation

---
+++ app/src/main/java/com/example/snowtam_pointet_vallee/Controller/FormController.java
@@ -0,0 +1,9 @@
+package com.example.snowtam_pointet_vallee.Controller;
+
+import com.example.snowtam_pointet_vallee.View.Formulaire;
+
+public class FormController {
+
+    private Formulaire formulaire;
+
+}
