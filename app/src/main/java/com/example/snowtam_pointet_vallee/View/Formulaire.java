d80bc0e7c41f18b16c7ce01fde574f35e0b20218
Lilian.V <lilian.vallee1108@gmail.com>
2020-11-13 15:33:04 +0100

    Package creation

---
+++ app/src/main/java/com/example/snowtam_pointet_vallee/View/Formulaire.java
@@ -0,0 +1,43 @@
+package com.example.snowtam_pointet_vallee.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.snowtam_pointet_vallee.R;
+
+public class Formulaire extends AppCompatActivity {
+
+    Intent formulaire = getIntent();
+    TextView airport1;
+    TextView airport2;
+    TextView airport3;
+    TextView airport4;
+
+    ImageButton show_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulaire);
+
+        airport1 = findViewById(R.id.airport1);
+        airport2 = findViewById(R.id.airport2);
+        airport3 = findViewById(R.id.airport3);
+        airport4 = findViewById(R.id.airport4);
+        show_result = findViewById(R.id.button_showResult);
+
+        show_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goto_result = new Intent(getApplicationContext(),ResultPage.class);
+                startActivity(goto_result);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/snowtam_pointet_vallee/View/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.snowtam_pointet_vallee.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.snowtam_pointet_vallee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button_newForm);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoFormulaire = new Intent(getApplicationContext(),Formulaire.class);
+                startActivity(gotoFormulaire);
+            }
+        });
+
+    }
+}
