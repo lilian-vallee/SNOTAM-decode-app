da498aa6beaa70ed464ac819675bf15a5d0a91b9
Lilian.V <lilian.vallee1108@gmail.com>
2020-11-13 15:43:59 +0100

    FormController + a little

--- app/src/main/java/com/example/snowtam_pointet_vallee/Controller/FormController.java
+++ app/src/main/java/com/example/snowtam_pointet_vallee/Controller/FormController.java
@@ -2,8 +2,18 @@ package com.example.snowtam_pointet_vallee.Controller;
 
 import com.example.snowtam_pointet_vallee.View.Formulaire;
 
+import java.util.ArrayList;
+
 public class FormController {
 
-    private Formulaire formulaire;
+    private Formulaire formPage;
+    private ArrayList listAeroport;
+
+
+    public FormController(Formulaire formPage){
+        this.formPage = formPage;
+    }
+
+
 
 }
