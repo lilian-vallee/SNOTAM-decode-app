689019931873e3bcfc52eb9b6f4e8a74542b8317
Pointet Nicolas <nicolas.91.pointet@hotmail.fr>
2020-11-16 11:05:19 +0100

    change formuaire 2/...

--- app/src/main/java/com/example/snowtam_pointet_vallee/Controller/FormController.java
+++ app/src/main/java/com/example/snowtam_pointet_vallee/Controller/FormController.java
@@ -39,7 +39,7 @@ public class FormController {
     public void setAirportsList(ArrayList code) {
             airportsList.add(code);
         }
-        
+
 
 
     }
--- app/src/main/java/com/example/snowtam_pointet_vallee/View/Formulaire.java
+++ app/src/main/java/com/example/snowtam_pointet_vallee/View/Formulaire.java
@@ -3,49 +3,74 @@ package com.example.snowtam_pointet_vallee.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.view.autofill.AutofillId;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.snowtam_pointet_vallee.Controller.FormController;
 import com.example.snowtam_pointet_vallee.R;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Formulaire extends AppCompatActivity {
 
     FormController controller = new FormController(this);
 
+    ArrayList codeAirport = new ArrayList();
+
     Intent formulaire = getIntent();
-    int airport1;
-    TextView airport2;
-    TextView airport3;
-    TextView airport4;
 
     ImageButton show_result;
+    ImageButton addAirport;
+
+    public ArrayList getAirportData () {
+
+        codeAirport.add(findViewById(R.id.airport1));
+        codeAirport.add(findViewById(R.id.airport2));
+        codeAirport.add(findViewById(R.id.airport3));
+        codeAirport.add(findViewById(R.id.airport4));
+        return codeAirport;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulaire);
 
+        int id = 0;
+
         FormController controller = new FormController(this);
 
-        airport1 = Integer.valueOf(String.valueOf(findViewById(R.id.airport1)));
-        airport2 = findViewById(R.id.airport2);
-        airport3 = findViewById(R.id.airport3);
-        airport4 = findViewById(R.id.airport4);
+
         show_result = findViewById(R.id.button_showResult);
+        addAirport = findViewById(R.id.button_addAirport);
+
+
+        addAirport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         show_result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                controller.addAirport(airport1.getText());//dynamique?
+                //method getAirportData => List(codeAirports)
+
+                controller.addAirport(getAirportData());
                 controller.RequeteAPI();
 
-                controller.setAirportsList(airport1);
+
+
 
             }
         });
